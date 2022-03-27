46b6c97104d367aa8c8f5e4596947ea19bac5dd9
alichowdhury01 <alichowdhury01@gmail.com>
2022-03-27 16:37:35 -0400

    Update Ali_labo2.py

--- Ali_labo2.py
+++ Ali_labo2.py
@@ -1,23 +1,47 @@
+from copy import copy
+from copy import deepcopy
+from encodings import utf_8
+
+
 # Étape 1
 
 # Chaque variable permet de lire leur base de donnée
+def  lecture_fichier():
+    f1 = open("data1.txt", "r", encoding='utf8')
+    f2 = open("data2.txt", "r", encoding='utf8')
+    f3 = open("data3.txt", "r", encoding='utf8')
+    read_line1 = f1.readlines()
+    read_line2 = f2.readlines()
+    read_line3 = f3.readlines()
+    read_line1[1] = float(read_line1[1])
+    read_line1[2] = float(read_line1[2])
+    read_line1[3] = float(read_line1[3])
+    read_line1[4] = float(read_line1[4])
+    read_line1[5] = float(read_line1[5])
+    read_line2[1] = float(read_line2[1])
+    read_line2[2] = float(read_line2[2])
+    read_line2[3] = float(read_line2[3])
+    read_line2[4] = float(read_line2[4])
+    read_line2[5] = float(read_line2[5])
+    read_line3[1] = float(read_line3[1])
+    read_line3[2] = float(read_line3[2])
+    read_line3[3] = float(read_line3[3])
+    read_line3[4] = float(read_line3[4])
+    read_line3[5] = float(read_line3[5])
+    liste = read_line1, read_line2, read_line3
+    f1.close()
+    f2.close()
+    f3.close()
+    return liste
 
-
+liste_equipe = lecture_fichier()
 
 
 # Étape 2
 
-def user_menu():
-    
-    print("1 pour Afficher les statistiques")
-    print("2 pour Sauvegarder les statistiques")
-    print("3 pour Afficher l'équipe avec les coureurs les plus similaires")
-    print("4 pour Ajouter une faute à une équipe")
-    return 
 
-user_menu()
 
-choix = int(input("Veuillez faire votre choix: "))
+
 
 # Étape 3
 
@@ -27,15 +51,16 @@ choix = int(input("Veuillez faire votre choix: "))
 #   * Temps moyens par équipe présenté en ordre croissant, donc de l'équipe la plus rapide à la plus lente. 
 #     Vous devez y afficher le nom de l'équipe et leur temps de course moyen.
 
-def afficher_stats():
+def execution_user(l_equipe):
 
-    f1 = open("data1.txt", "r", encoding='utf8')
-    #f2 = open("data2.txt", "r", encoding='utf8')
-    #f3 = open("data3.txt", "r", encoding='utf8')
-
+    print("1 pour Afficher les statistiques")
+    print("2 pour Sauvegarder les statistiques")
+    print("3 pour Afficher l'équipe avec les coureurs les plus similaires")
+    print("4 pour Ajouter une faute à une équipe")
     
+    choix = int(input("Veuillez faire votre choix: "))
 
-    if choix == 1:     
+    if choix == 1:
         
     elif choix == 2:
         # Sauvegarder les statistiques
@@ -50,4 +75,4 @@ def afficher_stats():
         print("Mauvais choix")
     return
 
-afficher_stats()
+execution_user(liste_equipe)
