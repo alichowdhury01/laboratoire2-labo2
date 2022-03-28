e1f1fc54b86cf8961a65ce9d5be09dc0fc8f628c
alichowdhury01 <alichowdhury01@gmail.com>
2022-03-28 17:34:37 -0400

    4

--- Ali_labo2.py
+++ Ali_labo2.py
@@ -3,82 +3,106 @@ from copy import deepcopy
 from encodings import utf_8
 
 
-
-# Étape 1
-
-# Chaque variable permet de lire leur base de donnée
 def  lecture_fichier():
     f1 = open("data1.txt", "r", encoding='utf8')
     f2 = open("data2.txt", "r", encoding='utf8')
     f3 = open("data3.txt", "r", encoding='utf8')
-    read_line1 = f1.readlines()
-    read_line2 = f2.readlines()
-    read_line3 = f3.readlines()
-    read_line1[1] = float(read_line1[1])
-    read_line1[2] = float(read_line1[2])
-    read_line1[3] = float(read_line1[3])
-    read_line1[4] = float(read_line1[4])
-    read_line1[5] = float(read_line1[5])
 
-    read_line2[1] = float(read_line2[1])
-    read_line2[2] = float(read_line2[2])
-    read_line2[3] = float(read_line2[3])
-    read_line2[4] = float(read_line2[4])
-    read_line2[5] = float(read_line2[5])
+    read_line_f1 = f1.readlines()
+    read_line_f2 = f2.readlines()
+    read_line_f3 = f3.readlines()
 
-    read_line3[1] = float(read_line3[1])
-    read_line3[2] = float(read_line3[2])
-    read_line3[3] = float(read_line3[3])
-    read_line3[4] = float(read_line3[4])
-    read_line3[5] = float(read_line3[5])
+    read_line_f1[1] = float(read_line_f1[1])
+    read_line_f1[2] = float(read_line_f1[2])
+    read_line_f1[3] = float(read_line_f1[3])
+    read_line_f1[4] = float(read_line_f1[4])
+    read_line_f1[5] = float(read_line_f1[5])
+
+    read_line_f2[1] = float(read_line_f2[1])
+    read_line_f2[2] = float(read_line_f2[2])
+    read_line_f2[3] = float(read_line_f2[3])
+    read_line_f2[4] = float(read_line_f2[4])
+    read_line_f2[5] = float(read_line_f2[5])
+
+    read_line_f3[1] = float(read_line_f3[1])
+    read_line_f3[2] = float(read_line_f3[2])
+    read_line_f3[3] = float(read_line_f3[3])
+    read_line_f3[4] = float(read_line_f3[4])
+    read_line_f3[5] = float(read_line_f3[5])
     
-    liste = read_line1, read_line2, read_line3
+    dictionnaire_equipe = {'A': read_line_f1[1:6], 'B': read_line_f2[1:6], 'C': read_line_f3[1:6]}
+
     f1.close()
     f2.close()
     f3.close()
-    return liste
+    return dictionnaire_equipe
 
 liste_equipe = lecture_fichier()
-
-
-# Étape 2
-
-
-
-
-
-# Étape 3
-
-# Vous devez afficher les statistiques suivantes :
-
-#   * Équipe ayant le coureur le plus rapide et le temps de ce dernier.
-#   * Temps moyens par équipe présenté en ordre croissant, donc de l'équipe la plus rapide à la plus lente. 
-#     Vous devez y afficher le nom de l'équipe et leur temps de course moyen.
+meilleur_temps = min(liste_equipe, key=liste_equipe.get)
 
 def execution_user(l_equipe):
 
-    print("1 pour Afficher les statistiques")
-    print("2 pour Sauvegarder les statistiques")
-    print("3 pour Afficher l'équipe avec les coureurs les plus similaires")
-    print("4 pour Ajouter une faute à une équipe")
-    
+    print("1 pour Afficher les statistiques\n"
+          "2 pour Sauvegarder les statistiques\n"
+          "3 pour Afficher l'équipe avec les coureurs les plus similaires\n"
+          "4 pour Ajouter une faute à une équipe")
+
     choix = int(input("Veuillez faire votre choix: "))
 
     if choix == 1:
-        min4 = min(liste_equipe[0:5])
-        min2 = min(liste_equipe[1:5])
-        min3 = min(liste_equipe[2:5])
-        totalmin = min([min2]), min([min4])
-        print(min(totalmin[1:5]))
+
+
+        equipeA_moyenne = f"{sum(liste_equipe['A'])/len(liste_equipe['A']):.3f}"
+        equipeB_moyenne = f"{sum(liste_equipe['B'])/len(liste_equipe['B']):.3f}"
+        equipeC_moyenne = f"{sum(liste_equipe['C'])/len(liste_equipe['C']):.3f}"
+
+        liste_moyenne = [equipeA_moyenne + ": Equipe A", equipeB_moyenne + ": Equipe B", \
+                         equipeC_moyenne + ": Equipe C"]
+
+        print(f"\nLe meilleur coureur est dans l'équipe {meilleur_temps} et son temps"
+              f" étant de {min(liste_equipe[meilleur_temps])}\n")
+          
+        print(f"Moyenne des equipes: {sorted(liste_moyenne)}")
     elif choix == 2:
-        # Sauvegarder les statistiques
-        print("jdjd")
+        equipeA_moyenne = f"{sum(liste_equipe['A'])/len(liste_equipe['A']):.3f}"
+        equipeB_moyenne = f"{sum(liste_equipe['B'])/len(liste_equipe['B']):.3f}"
+        equipeC_moyenne = f"{sum(liste_equipe['C'])/len(liste_equipe['C']):.3f}"
+
+        liste_moyenne = [equipeA_moyenne + ": Equipe A", equipeB_moyenne + ": Equipe B", \
+                         equipeC_moyenne + ": Equipe C"]
+        liste_moyenne_croissant= sorted(liste_moyenne)
+
+        sauvegarde_stat = open("stats.txt", "w", encoding='utf8')
+        sauvegarde_stat.write(f"{meilleur_temps}")
+        sauvegarde_stat.write(f"Moyennes des equipes: \n"
+                              f"{liste_moyenne_croissant}")
+        sauvegarde_stat.close()
+
+        
     elif choix == 3:
         # Afficher l'équipe avec les coureurs les plus similaires
         print("jddj")
+        
     elif choix == 4:
-        # Ajouter une faute à une équipe
-        print("sjs")
+
+        ajout_faute = open("stats.txt", "a", encoding='utf8')
+
+        selection_equipe_faute = int(input("Liste des équipes: \n"
+                                           "1 pour Équipe A \n"
+                                           "2 pour Équipe B \n"
+                                           "3 pour Équipe C \n"
+                                           "4 pour Toutes les équipes \n"
+                                           "Choisissez pour qui voulez-vous attrier une faute?:"))
+        if selection_equipe_faute == 1:
+            ajout_faute.write("\n1 faute pour l'équipe A")
+        elif selection_equipe_faute == 2:
+            ajout_faute.write("\n1 faute pour l'équipe B")
+        elif selection_equipe_faute == 3:
+            ajout_faute.write("\n1 faute pour l'équipe C")
+        elif selection_equipe_faute == 4:
+            ajout_faute.write("\n1 faute pour toutes les équipes")
+        else:
+            print("Entrez invalide")
     else:
         print("Mauvais choix")
     return
